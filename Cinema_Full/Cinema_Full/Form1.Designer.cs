75407192b664d9aa88ea8ee54e78096583745201
AnarAbishov <mr.avinar@mail.ru>
2017-11-13 18:06:33 +0400

    Add files via upload

---
+++ Cinema_Full/Cinema_Full/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace Cinema_Full
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbxFilms = new System.Windows.Forms.ComboBox();
+            this.dgvFilms = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbxHalls = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFilms)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cmbxFilms
+            // 
+            this.cmbxFilms.FormattingEnabled = true;
+            this.cmbxFilms.Location = new System.Drawing.Point(12, 49);
+            this.cmbxFilms.Name = "cmbxFilms";
+            this.cmbxFilms.Size = new System.Drawing.Size(172, 24);
+            this.cmbxFilms.TabIndex = 0;
+            this.cmbxFilms.SelectedIndexChanged += new System.EventHandler(this.cmbxFilms_SelectedIndexChanged);
+            // 
+            // dgvFilms
+            // 
+            this.dgvFilms.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvFilms.Location = new System.Drawing.Point(12, 200);
+            this.dgvFilms.Name = "dgvFilms";
+            this.dgvFilms.RowTemplate.Height = 24;
+            this.dgvFilms.Size = new System.Drawing.Size(799, 215);
+            this.dgvFilms.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(183, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Zehmet olmasa Film sechin:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(136, 129);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 31);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Yerleri Secmek";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 177);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(169, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Filmler haqqinda melumat";
+            // 
+            // cmbxHalls
+            // 
+            this.cmbxHalls.FormattingEnabled = true;
+            this.cmbxHalls.Location = new System.Drawing.Point(263, 49);
+            this.cmbxHalls.Name = "cmbxHalls";
+            this.cmbxHalls.Size = new System.Drawing.Size(182, 24);
+            this.cmbxHalls.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(263, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Zali sechin";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(823, 437);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cmbxHalls);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgvFilms);
+            this.Controls.Add(this.cmbxFilms);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFilms)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbxFilms;
+        private System.Windows.Forms.DataGridView dgvFilms;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbxHalls;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Cinema_Full/Cinema_Full/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Cinema_Full.Models;
+
+namespace Cinema_Full
+{
+    public partial class Form1 : Form
+    {
+        private CinemaEntities db = new CinemaEntities();
+        public Form1()
+        {
+            InitializeComponent();
+            Display();
+            fillFilms();
+            fillHalls();
+        }
+
+        private void Display()
+        {
+            dgvFilms.DataSource = db.Films.Select(f => new {f.Name, f.MovieLength, f.ReleaseYear, f.Language, f.Rejissor,f.Janrs }).ToList();
+        }
+
+       
+        private void fillFilms()
+        {
+            foreach (Film item in db.Films.ToList())
+            {
+                cmbxFilms.Items.Add(item.Name);
+            }
+        }
+
+        private void fillHalls()
+        {
+            foreach (Hall item in db.Halls.ToList())
+            {
+                cmbxHalls.Items.Add(item.Name);
+            }
+        }
+
+        private void cmbxFilms_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string films = cmbxFilms.Text;
+            if (films == string.Empty)
+            {
+                MessageBox.Show("Filmi seçin");
+                return;
+            }
+
+            cmbxHalls.Items.Clear();
+            cmbxHalls.Text = "";
+            
+
+            int filmId = this.getCountryIdByName(country);
+
+            List<Film> cities = db.Films.Where(c => c.CountryId == countryId).ToList();
+
+            foreach (Film item in cities)
+            {
+                cmbxFilms.Items.Add(item.Name);
+            }
+        }
+         private int getCountryIdByName(string filmsName)
+        {
+            return db.Films.FirstOrDefault(c => c.Name == filmsName).Id;
+        }
+    }
+}
